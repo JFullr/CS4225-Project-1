26c7043a4a3bb54c6491607199fe39d18b2b20d3
Timothy Brooks <tbrook11@my.westga.edu>
2020-03-04 13:22:06 -0500

    added some javadocs, checkstyle fixes, and extra demo

--- src/project/Gui.java
+++ src/project/Gui.java
@@ -2,14 +2,21 @@ package project;
 
 import javax.swing.JFrame;
 
+/**
+ * The Class Gui.
+ * 
+ * @author Joseph Fuller, James Irwin, Timothy Brooks
+ * @version Spring 2020
+ */
 public class Gui {
-	
+
 	private JFrame j;
-	
+
+	/**
+	 * Instantiates a new gui.
+	 */
 	public Gui() {
 		//
 	}
-	
-	
-	
+
 }
--- src/project/Main.java
+++ src/project/Main.java
@@ -6,61 +6,77 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
+/**
+ * The Class Main.
+ *
+ * @author csuser
+ */
 public class Main {
-	
+
 	private static int width = 800;
 	private static int height = 600;
+	
 	private static JFrame j;
 	private static boolean running;
 	private static Image i;
-	
+
+	/**
+	 * The main method begins a new game.
+	 *
+	 * @param args not used
+	 */
 	public static void main(String[] args) {
-		
+
 		j = new JFrame("Ultimate Aussue Road Snake Simulator");
-		j.getContentPane().setPreferredSize(new Dimension(width,height));
+		j.getContentPane().setPreferredSize(new Dimension(width, height));
 		j.pack();
 		j.setResizable(false);
 		j.pack();
 		j.setVisible(true);
 		j.setDefaultCloseOperation(3);
-		
-		new Thread(()->{
+
+		new Thread(() -> {
 			graphics();
 		}).start();
-		
+
 	}
-	
+
 	private static void graphics() {
-		
+
 		running = true;
 		i = j.createVolatileImage(width, height);
-		if(i == null) {
+		if (i == null) {
 			return;
 		}
-		
-		while(running) {
+
+		while (running) {
 			try {
-				
+
 				Graphics g = i.getGraphics();
 				g.setColor(Color.BLACK);
 				g.fillRect(0, 0, width, height);
 				g.setColor(Color.GREEN);
-				 
+
 				g.setFont(new Font("TimesRoman", Font.PLAIN, 32));
 				g.drawString("Hello World", 30, 30);
+				g.drawString("this is what happens when you burn the \"Land down under\"", 30, 80);
+
+				ImageIcon halo = new ImageIcon("BS-883200.jpg");
+				g.drawImage(halo.getImage(), (width / 2) - 225, 100, 450, 450, null);
 				
 				g = j.getContentPane().getGraphics();
 				g.drawImage(i, 0, 0, null);
 				g.dispose();
-				
+
 				Thread.sleep(50);
-			}catch(Exception e) {
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
-		
+
 	}
 
 }
