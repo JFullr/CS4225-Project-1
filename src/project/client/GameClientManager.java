218a400db2eef11d7a7d62e829fb70e094cdd0c7
James Irwin <jirwin2@my.westga.edu>
2020-03-07 10:17:04 -0500

    Added Checkstyle to GameClientManager.

--- src/project/client/GameClientManager.java
+++ src/project/client/GameClientManager.java
@@ -1,129 +1,149 @@
 package project.client;
 
+/**
+ * Manages a GameClient's interactions through the network.
+ * 
+ * @author Joseph Fuller, James Irwin, Timothy Brooks
+ * @version Spring 2020
+ */
 public class GameClientManager {
-	
+
 	private static final int ATTEMPT_TIMEOUT_MILLIS = 10000;
-	
+
 	private static final int HEARBEAT_TIMEOUT_MILLIS = 5000;
 	private static final String ADDRESS = "127.0.0.1";
 	private static final int PORT = 4225;
-	
+
 	private GameClient client;
-	
+
 	private volatile boolean running;
 	private boolean attempt;
-	
+
+	/**
+	 * Instantiates a new GameClientManager with a GameClient.
+	 */
 	public GameClientManager() {
-		
-		this.client = new GameClient(ADDRESS,PORT);
+
+		this.client = new GameClient(ADDRESS, PORT);
 		this.attempt = false;
-		
+
 	}
-	
+
+	/**
+	 * Stops the GameClientManager
+	 */
 	public void end() {
 		this.attempt = false;
 		this.running = false;
 	}
-	
+
+	/**
+	 * Entry-point of the GameClientManager/Begins the GameClientManager
+	 * 
+	 * @return false if GameClientManager fails to connect.
+	 */
 	public boolean start() {
-		
-		synchronized(this) {
-			
+
+		synchronized (this) {
+
 			this.makeAutoAttemptThread();
-		
-			if(this.running) {
+
+			if (this.running) {
 				return true;
 			}
-			
-			if(!this.client.connectBlocking()) {
+
+			if (!this.client.connectBlocking()) {
 				return false;
 			}
-			
+
 			this.running = true;
-			
+
 			this.makeNetworkThreads();
-			
+
 			return true;
-		
+
 		}
-		
+
 	}
-	
+
+	/**
+	 * Returns whether or not the GameClientManager is running.
+	 * 
+	 * @return true if running, false if not running
+	 */
 	public boolean isRunning() {
 		return this.running;
 	}
-	
+
 	private void networkProcess() {
-		
-		
-		
+
 	}
-	
+
 	private void makeAutoAttemptThread() {
-		
-		if(this.attempt) {
+
+		if (this.attempt) {
 			return;
 		}
-		synchronized(this) {
-			
-			if(this.attempt) {
+		synchronized (this) {
+
+			if (this.attempt) {
 				return;
 			}
 			this.attempt = true;
-			
-			new Thread(()->{
-				
-				while(this.attempt) {
-					
+
+			new Thread(() -> {
+
+				while (this.attempt) {
+
 					try {
 						Thread.sleep(ATTEMPT_TIMEOUT_MILLIS);
-					} catch (InterruptedException e) {}
-					
-					if(!this.isRunning()) {
+					} catch (InterruptedException e) {
+					}
+
+					if (!this.isRunning()) {
 						this.start();
 					}
 				}
-				
+
 			}).start();
-			
+
 		}
 	}
-	
+
 	private void makeNetworkThreads() {
-		new Thread(()->{
-			
-			while(this.running) {
-				
+		new Thread(() -> {
+
+			while (this.running) {
 				try {
 					Thread.sleep(HEARBEAT_TIMEOUT_MILLIS);
-				} catch (InterruptedException e) {}
-				
+				} catch (InterruptedException e) {
+				}
+
 				this.client.sendHeartbeat();
 			}
-			
-			while(!this.client.close()) {
+			while (!this.client.close()) {
 				this.client.close();
 			}
-			
 		}).start();
-		
-		new Thread(()->{
-			
-			while(this.running) {
-				
+
+		new Thread(() -> {
+
+			while (this.running) {
+
 				this.networkProcess();
-				
+
 				try {
 					Thread.sleep(1);
-				} catch (InterruptedException e) {}
-				
+				} catch (InterruptedException e) {
+				}
+
 			}
-			
-			while(!this.client.close()) {
+
+			while (!this.client.close()) {
 				this.client.close();
 			}
-			
+
 		}).start();
 	}
-	
+
 }
