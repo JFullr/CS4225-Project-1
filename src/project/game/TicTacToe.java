f7a271f77dcde316fc43032989461da3905bb348
James Irwin <jirwin2@my.westga.edu>
2020-03-08 19:31:48 -0400

    Added Diagonal win check functionality to TicTacToe.

--- src/project/game/TicTacToe.java
+++ src/project/game/TicTacToe.java
@@ -1,5 +1,6 @@
 package project.game;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
@@ -15,6 +16,9 @@ public class TicTacToe {
 	private static final String EMPTYSPACE = "_";
 	private static final int NUMBEROFPLAYERS = 2;
 
+	private static final int NUMBEROFNONDIAGONALS = 4;
+	private static final int DIAGONALSIZE = 3;
+
 	private static final String R1C1 = "A1";
 	private static final String R1C2 = "A2";
 	private static final String R1C3 = "A3";
@@ -29,6 +33,7 @@ public class TicTacToe {
 
 	private HashMap<String, String> coordinateMap = new HashMap<String, String>();
 	private String[] playerNames;
+	private ArrayList<String> nonDiagonal;
 
 	/**
 	 * Instantiates an empty TicTacToe board.
@@ -38,6 +43,12 @@ public class TicTacToe {
 		this.playerNames = new String[NUMBEROFPLAYERS];
 		this.playerNames[0] = PLAYER1;
 		this.playerNames[1] = PLAYER2;
+
+		this.nonDiagonal = new ArrayList<String>();
+		this.nonDiagonal.add(R1C2);
+		this.nonDiagonal.add(R2C1);
+		this.nonDiagonal.add(R2C3);
+		this.nonDiagonal.add(R3C2);
 
 		this.coordinateMap.put(R1C1, EMPTYSPACE);
 		this.coordinateMap.put(R1C2, EMPTYSPACE);
@@ -59,9 +70,16 @@ public class TicTacToe {
 	 * @param coordinate the coordinate the player has selected.
 	 */
 	public void inputCoordinate(String playerName, String coordinate) {
+		String winningPlayer = null;
+
 		if (this.isValidCoordinate(coordinate) && this.isValidPlayerName(playerName)) {
 			this.coordinateMap.put(coordinate, playerName);
 		}
+
+		if (!this.nonDiagonal.contains(coordinate)) {
+			winningPlayer = this.isDiagonalWin(playerName);
+		}
+
 	}
 
 	private boolean isValidCoordinate(String coordinate) {
@@ -82,6 +100,46 @@ public class TicTacToe {
 			}
 		}
 		return answer;
+	}
+
+	private String isDiagonalWin(String playerName) {
+		String winningPlayer = playerName;
+
+		ArrayList<String> leftDiagonal = this.assembleLeftDiagonal();
+		ArrayList<String> rightDiagonal = this.assembleRightDiagonal();
+
+		for (String current : leftDiagonal) {
+			if (!current.equals(playerName)) {
+				winningPlayer = null;
+			}
+		}
+		for (String current : rightDiagonal) {
+			if (!current.equals(playerName)) {
+				winningPlayer = null;
+			}
+		}
+
+		return winningPlayer;
+	}
+
+	private ArrayList<String> assembleLeftDiagonal() {
+		ArrayList<String> leftDiagonal = new ArrayList<String>();
+
+		leftDiagonal.add(this.coordinateMap.get(R1C1));
+		leftDiagonal.add(this.coordinateMap.get(R2C2));
+		leftDiagonal.add(this.coordinateMap.get(R3C3));
+
+		return leftDiagonal;
+	}
+
+	private ArrayList<String> assembleRightDiagonal() {
+		ArrayList<String> rightDiagonal = new ArrayList<String>();
+
+		rightDiagonal.add(this.coordinateMap.get(R1C3));
+		rightDiagonal.add(this.coordinateMap.get(R2C2));
+		rightDiagonal.add(this.coordinateMap.get(R3C1));
+
+		return rightDiagonal;
 	}
 
 }
