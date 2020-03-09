b982198c19fd30e7a649840d6ebc2b041792cd43
James Irwin <jirwin2@my.westga.edu>
2020-03-09 12:10:59 -0400

    Added TicTacToeManager class.

---
+++ src/project/game/TicTacToeManager.java
@@ -0,0 +1,20 @@
+package project.game;
+
+/**
+ * Coordinates use of TicTacToe class to User Interface.
+ * 
+ * @author Joseph Fuller, James Irwin, Timothy Brooks
+ * @version Spring 2020
+ */
+public class TicTacToeManager {
+
+	private TicTacToe aTicTacToe;
+
+	/**
+	 * Instantiates a TicTacToeManager with an unused TicTacToe class.
+	 */
+	public TicTacToeManager() {
+		this.aTicTacToe = new TicTacToe();
+	}
+
+}
