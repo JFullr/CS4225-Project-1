00e31ad4bcd107b7956b1f37f36bb51dcfea747e
Timothy Brooks <tbrook11@my.westga.edu>
2020-03-11 14:12:21 -0400

    fixed most of checkstyle errors

--- src/AurtdrsGameServer.java
+++ src/AurtdrsGameServer.java
@@ -1,74 +1,93 @@
 
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import project.server.GameServerManager;
 import utils.network.Client;
 
+/**
+ * The Class AurtdrsGameServer.
+ *
+ * @author Joseph Fuller, James Irwin, Timothy Brooks
+ * @version Spring 2020
+ */
 public class AurtdrsGameServer {
-	
+
 	private GameServerManager server;
-	
 	private HashMap<ArrayList<Client>, AurtdrsServerProcess> gameProcesses;
 
+	/**
+	 * Instantiates a new aurtdrs game server.
+	 *
+	 * @param port the port
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
 	public AurtdrsGameServer(int port) throws IOException {
 		this.server = new GameServerManager(port);
 	}
-	
+
+	/**
+	 * Start.
+	 */
 	public void start() {
 		this.server.start();
 		this.gameProcess();
 	}
-	
+
+	/**
+	 * End.
+	 */
 	public void end() {
 		this.server.end();
 	}
-	
+
+	/**
+	 * Game process.
+	 */
 	public void gameProcess() {
-		
-		new Thread(()->{
-			
-			while(true) {
-				
+
+		new Thread(() -> {
+
+			while (true) {
+
 				this.setGamePools();
 				this.processGames();
-				
+
 				try {
 					Thread.sleep(1);
 				} catch (InterruptedException e) {
 				}
-				
+
 			}
-			
-		}).start(); 
-		
-		
-		
+
+		}).start();
+
 	}
-	
+
 	private void setGamePools() {
-		
-		for(ArrayList<Client> pool : this.server.getGamePools()) {
-			
-			if(!this.gameProcesses.keySet().contains(pool)) {
+
+		for (ArrayList<Client> pool : this.server.getGamePools()) {
+
+			if (!this.gameProcesses.keySet().contains(pool)) {
 				this.gameProcesses.put(pool, new AurtdrsServerProcess(pool));
 			}
-			
+
 		}
-		
+
 	}
-	
+
+	/**
+	 * Process games.
+	 */
 	public void processGames() {
-		
-		for(ArrayList<Client> pool : this.server.getGamePools()) {
-			
+
+		for (ArrayList<Client> pool : this.server.getGamePools()) {
+
 			this.gameProcesses.get(pool).processGame();
-			
-			
+
 		}
-		
+
 	}
-	
-	
+
 }
--- src/XClient.java
+++ src/XClient.java
@@ -2,23 +2,33 @@ import project.client.GameClientManager;
 import project.client.NetworkData;
 import project.client.NetworkGameState;
 
+/**
+ * The Class XClient.
+ *
+ * @author Joseph Fuller, James Irwin, Timothy Brooks
+ * @version Spring 2020
+ */
 public class XClient {
-	
-	private GameClientManager client;
-	
-	public XClient() {
-		
-		client = new GameClientManager();
-		client.start();
-		
-	}
-	
-	public connect() {
 
-		client.sendData(new NetworkData(NetworkGameState.LOBBY,"connect"));
-		
+	private GameClientManager client;
+
+	/**
+	 * Instantiates a new x client.
+	 */
+	public XClient() {
+
+		this.client = new GameClientManager();
+		this.client.start();
+
 	}
-	
-	
-	
+
+	/**
+	 * Connect.
+	 */
+	public void connect() {
+
+		this.client.sendData(new NetworkData(NetworkGameState.LOBBY, "connect"));
+
+	}
+
 }
